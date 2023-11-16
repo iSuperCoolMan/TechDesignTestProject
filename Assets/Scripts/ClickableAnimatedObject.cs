a406344549e78568bf9a1421934816be8b1290c3
SuperCoolMan <101731621+iSuperCoolMan@users.noreply.github.com>
2023-11-16 23:37:19 +0400

    Added localisation and sound

--- Assets/Scripts/ClickableAnimatedObject.cs
+++ Assets/Scripts/ClickableAnimatedObject.cs
@@ -2,18 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(AudioSource))]
 [RequireComponent(typeof(BoxCollider2D))]
 public class ClickableAnimatedObject : MonoBehaviour
 {
     private Animator _animator;
+    private AudioSource _actionSound;
 
     private void Awake()
     {
         _animator = GetComponent<Animator>();
+        _actionSound = GetComponent<AudioSource>();
     }
 
     private void OnMouseDown()
     {
         _animator.SetTrigger("Clicked");
+        _actionSound.Play();
     }
 }
